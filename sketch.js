c7f541e63ae876b749fe90bb98a6cd96986dc47d
nitattt34-boop <nitattt34@gmail.com>
2026-05-11 11:08:18 +0800

    1.2

--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
 let video;
-let facemesh;
-let predictions = [];
+let faceMesh;
+let faces = [];
+
+function preload() {
+  // 載入 ml5.js v1 的 FaceMesh 模型
+  faceMesh = ml5.faceMesh();
+}
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(width, height);
   
-  // 載入 ml5.js 的 Facemesh 模型
-  facemesh = ml5.facemesh(video, modelReady);
-  
-  // 當辨識到人臉時，將結果儲存到 predictions 陣列
-  facemesh.on("predict", results => {
-    predictions = results;
-  });
-  
   video.hide(); // 隱藏原本自動生成的 HTML video 元素
+  
+  // 啟動人臉辨識，並將結果傳給 gotFaces 函式
+  faceMesh.detectStart(video, gotFaces);
 }
 
-function modelReady() {
-  console.log("Facemesh 模型載入完成！");
+function gotFaces(results) {
+  faces = results;
 }
 
 function draw() {
@@ -27,28 +27,29 @@ function draw() {
   image(video, 0, 0, width, height);
   
   // 迴圈處理每一個辨識到的人臉
-  for (let i = 0; i < predictions.length; i += 1) {
-    const keypoints = predictions[i].scaledMesh;
+  for (let i = 0; i < faces.length; i++) {
+    const face = faces[i];
     
     // MediaPipe Facemesh 中：
     // 177 為左臉最下緣外側，401 為右臉最下緣外側，這兩個點最接近耳垂位置
-    const leftEarlobe = keypoints[177];
-    const rightEarlobe = keypoints[401];
+    // 注意：ml5 v1 的節點結構改為 {x, y, z}，需用 .x 與 .y 取得座標
+    const leftEarlobe = face.keypoints[177];
+    const rightEarlobe = face.keypoints[401];
     
     // 繪製左耳環 (垂墜樣式)
     stroke(200); // 銀色的耳鍊
     strokeWeight(2);
-    line(leftEarlobe[0], leftEarlobe[1], leftEarlobe[0], leftEarlobe[1] + 30);
+    line(leftEarlobe.x, leftEarlobe.y, leftEarlobe.x, leftEarlobe.y + 30);
     fill(0, 255, 255); // 青色的寶石
     noStroke();
-    circle(leftEarlobe[0], leftEarlobe[1] + 30, 15);
+    circle(leftEarlobe.x, leftEarlobe.y + 30, 15);
 
     // 繪製右耳環 (垂墜樣式)
     stroke(200); 
     strokeWeight(2);
-    line(rightEarlobe[0], rightEarlobe[1], rightEarlobe[0], rightEarlobe[1] + 30);
+    line(rightEarlobe.x, rightEarlobe.y, rightEarlobe.x, rightEarlobe.y + 30);
     fill(0, 255, 255); 
     noStroke();
-    circle(rightEarlobe[0], rightEarlobe[1] + 30, 15);
+    circle(rightEarlobe.x, rightEarlobe.y + 30, 15);
   }
 }
